3f7b5efe3f392938f55b59864fa06faaf9887756
최빛찬 <@PC>
2016-11-21 23:30:37 +0900

    11.21

--- src/com/chan/home_study/Study_.java
+++ src/com/chan/home_study/Study_.java
@@ -3,5 +3,36 @@ package com.chan.home_study;
 public class Study_ {
 	public static void main(String[] args) {
 		
+//		int i = 10;
+//		double d = i;
+//		
+//		double d2 = 3.14;
+//		int i2 = (int)d2;
+//		
+//		System.out.println(d2);
+//		System.out.println(i2);
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("숫자를 입력하세요. : ");
+//		int i = sc.nextInt();
+				
+//		System.out.println(i+"를 입력하셨습니다.");
+		
+//		if((i%3)==0){
+//			System.out.println(i+"는 3의 배수입니다");
+//		} else if((i%3)!=0) {
+//			System.out.println(i+"는 3의 배수가 아닙니다");
+//		}
+		
+		int i = 1;
+		while (i <= 100){
+			
+			if((i%3)==0){
+				System.out.println(i +", " + (i+i));
+			}
+			
+			i++;
+			
+		} 
 	}
 }
