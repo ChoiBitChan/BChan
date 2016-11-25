2b8cc84ca9661a52e1eaea569483484861aea576
B_Chan <lucete1104@gmail.com>
2016-11-25 21:27:08 +0900

    11.25

---
+++ src/com/chan/home_study/Study_Thread_GBB.java
@@ -0,0 +1,36 @@
+package com.chan.home_study;
+
+public class Study_Thread_GBB extends Thread {
+
+	String name;
+	int result;
+
+	public Study_Thread_GBB(String name) {
+		this.name = name;
+	}
+
+	public void run() {
+
+		try {
+
+			int gbb_random = (int) (Math.random() * 3) + 1;
+			result = gbb_random;
+
+			if (gbb_random == 1) {
+				System.out.println(name + " : 가위");
+			}
+			if (gbb_random == 2) {
+				System.out.println(name + " : 바위");
+			}
+			if (gbb_random == 3) {
+				System.out.println(name + " : 보");
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
